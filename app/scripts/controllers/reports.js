1fb90fdf7d91731093ae9f512b2a7cc6ce4653a4
Daniel Naranjo <dnaranjo@gmail.com>
2015-05-15 11:01:26 -0430

    Fixes

--- app/scripts/controllers/reports.js
+++ app/scripts/controllers/reports.js
@@ -7,12 +7,12 @@
  * # ReportsCtrl
  * Controller of the calculatorApp
  */
-app.controller('ReportsCtrl', function ($scope, Data, $http, auth, $cookies) {
+app.controller('ReportsCtrl', function ($scope, Data, $http, auth, $cookies, toaster) {
 
-	console.log('cookie: ',$cookies);
+	//console.log('cookie: ',$cookies);
 	$scope.uID = $cookies.uID;
     $scope.taxes = $cookies.taxes;
-	$scope.partner = $cookies.partner ;
+	$scope.partner = $cookies.partner;
 	$scope.type = $cookies.type;
   
     var d = new Date();
@@ -27,5 +27,24 @@ app.controller('ReportsCtrl', function ($scope, Data, $http, auth, $cookies) {
     .error(function(){
         console.log('Error API lawyers');
     });
+
+    $scope.custom = true;
+    $scope.filter={};
+    $scope.filter.uID=$cookies.uID;
+    $scope.execute = function() {
+        $http({
+            method:'GET',
+            url:'http://apps-lean.com/api/v1/lawyer/splitbydaterange/'+$scope.filter.uID+'/'+$scope.filter.in+'/'+$scope.filter.out,
+            data:$.param($scope.filter)
+            // headers:{ 'Content-Type': 'application/x-www-form-urlencoded' }
+        })        
+        .success(function(data){
+            $scope.reports = data;
+            console.log('data:',$scope.filter);
+        }).error(function(data){
+            toaster.pop('error', '', 'Error we can not complete this action', 5000, 'trustedHtml');
+            console.log('error:',data);
+        });
+    };
 
   });
