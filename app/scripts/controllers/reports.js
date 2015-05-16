38b91b85affc2db0ebe0531ee99c143b6426df66
Daniel Naranjo <dnaranjo@gmail.com>
2015-05-16 15:11:01 -0430

    Date range, hide debug functions

--- app/scripts/controllers/reports.js
+++ app/scripts/controllers/reports.js
@@ -7,7 +7,7 @@
  * # ReportsCtrl
  * Controller of the calculatorApp
  */
-app.controller('ReportsCtrl', function ($scope, Data, $http, auth, $cookies, toaster) {
+app.controller('ReportsCtrl', function ($scope, Data, $http, auth, $cookies, toaster, $window) {
 
 	//console.log('cookie: ',$cookies);
 	$scope.uID = $cookies.uID;
@@ -28,23 +28,31 @@ app.controller('ReportsCtrl', function ($scope, Data, $http, auth, $cookies, toa
         console.log('Error API lawyers');
     });
 
-    $scope.custom = true;
+    $scope.custom = true;// abierto = true cerrado=false
     $scope.filter={};
     $scope.filter.uID=$cookies.uID;
+    // $scope.filter.in= new Date();//'04/01/2015T04:30:00.000Z'
+    // $scope.filter.out= new Date();//'30/01/2015T04:30:00.000Z'
     $scope.execute = function() {
         $http({
-            method:'GET',
-            url:'http://apps-lean.com/api/v1/lawyer/splitbydaterange/'+$scope.filter.uID+'/'+$scope.filter.in+'/'+$scope.filter.out,
-            data:$.param($scope.filter)
-            // headers:{ 'Content-Type': 'application/x-www-form-urlencoded' }
+            method:'POST',
+            url:'http://apps-lean.com/api/v1/lawyer/splitbydaterange', //'+$scope.filter.uID+'/'+$scope.filter.in+'/'+$scope.filter.out
+            data:$.param($scope.filter),
+            headers:{ 'Content-Type': 'application/x-www-form-urlencoded' }
         })        
         .success(function(data){
-            $scope.reports = data;
-            console.log('data:',$scope.filter);
+            $scope.range = data;
+            console.log('Sent: ',$scope.filter);
         }).error(function(data){
             toaster.pop('error', '', 'Error we can not complete this action', 5000, 'trustedHtml');
             console.log('error:',data);
         });
     };
 
+    $scope.refresh = function () {
+        $window.location.reload();
+        // $route.reload();
+    };
+//
+//
   });
